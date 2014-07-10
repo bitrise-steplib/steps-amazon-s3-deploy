9174d9019c0b35dff447a9c7171cc5e3bf266151
Tamas 4d <tamas.szucs@4dpipeline.com>
2014-07-10 18:39:45 +0200

    test file changed, but ruby cannot be tested. TODO

--- tests/step_test.sh
+++ tests/step_test.sh
@@ -83,10 +83,11 @@ function is_not_empty {
   fi
 }
 
-function test_cleanup {
+function test_env_cleanup {
   unset S3_DEPLOY_AWS_ACCESS_KEY
 	unset S3_DEPLOY_AWS_SECRET_KEY
 	unset S3_BUCKET_NAME
+  unset CONCRETE_IPA_PATH
 }
 
 function print_new_test {
@@ -94,12 +95,13 @@ function print_new_test {
   echo "[TEST]"
 }
 
+function run_target_command { 
+  print_and_do_command ruby ./s3deploy.rb
+}
+
+
 # -----------------
 # --- Run tests ---
-
-function run_target_command { 
-  print_and_do_command eval "CONCRETE_IPA_PATH=$test_ipa_path ./step.sh"
-}
 
 echo "Starting tests..."
 
@@ -107,3 +109,26 @@ test_ipa_path="tests/testfile.ipa"
 test_results_success_count=0
 test_results_error_count=0
 
+# [TEST] Call the command with S3_DEPLOY_AWS_ACCESS_KEY not set, 
+# it should raise an error message and exit
+# 
+(
+  print_new_test
+  test_env_cleanup
+
+  # Set env vars
+  export S3_DEPLOY_AWS_SECRET_KEY="dsa4321"
+  export S3_BUCKET_NAME="dsa4321"
+  export CONCRETE_IPA_PATH="$test_ipa_path"
+
+  # All env vars should exist except TWILIO_ACCOUNT_SID
+  expect_error "TWILIO_ACCOUNT_SID environment variable should NOT be set" is_not_empty "$TWILIO_ACCOUNT_SID"
+  expect_success "S3_DEPLOY_AWS_SECRET_KEY environment variable should be set" is_not_empty "$S3_DEPLOY_AWS_SECRET_KEY"
+  expect_success "S3_BUCKET_NAME environment variable should be set" is_not_empty "$S3_BUCKET_NAME"
+  expect_success "CONCRETE_IPA_PATH environment variable should be set" is_not_empty "$CONCRETE_IPA_PATH"
+
+  # Send sms request
+  expect_error "The command should be called, but should not complete sucessfully" run_target_command
+)
+test_result=$?
+inspect_test_result $test_result
