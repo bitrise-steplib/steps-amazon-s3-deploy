1a0dc05e15ed946ec00b51385af8e2ccf1f5818c
Viktor Benei <viktor.benei@gmail.com>
2015-01-23 11:10:03 +0100

    new s3cmd (1.5.0) OS X fix & step.yml update

--- step.sh
+++ step.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# s3cmd OS X fix
+bash "${THIS_SCRIPT_DIR}/__s3cmd_osx_fix.sh"
+if [ $? -ne 0 ] ; then
+	echo "[!] Failed to apply required s3cmd fix"
+	exit 1
+fi
+
 ruby ./s3deploy.rb
 exit $?
